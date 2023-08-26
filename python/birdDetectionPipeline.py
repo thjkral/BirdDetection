bc64ae02927d9d3b54b3f739a5bfb6f482c5ff6b
thjkral <thjkral@gmail.com>
2023-08-26 10:27:24 +0200

    Logging both printed to files and console.

--- python/birdDetectionPipeline.py
+++ python/birdDetectionPipeline.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 """
 Starting point for the pipeline that runs every night. It has four main functions:
 1) Add cached photos to the database
@@ -24,7 +25,6 @@ def read_config():  # Load config
     """Open and load the config"""
     try:
         with open('/etc/birdconfig/birdconfig.json', 'r') as f:
-            print('Config file loaded')
             return json.load(f)
     except Error as e:
         print(e)
@@ -43,7 +43,6 @@ parser.add_argument('-i', '--identify', action='store_true', help='Identify new 
 parser.add_argument('-v', '--visits', action='store_true', help='Subdivide new images in visits')
 parser.add_argument('-r', '--recap', action='store_true', help='Send daily summaries of the process')
 parser.add_argument('-a', '--all', action='store_true', help='Run the entire pipeline')
-parser.add_argument('-l', '--log', action='store_true', help='Create log file')
 
 args = parser.parse_args()
 
@@ -54,15 +53,16 @@ if len(sys.argv) == 1:  # quit if no arguments are provided
 else:  # execute program if arguments are passed
 
     # Set up logging
-    if args.log:
-        logging.basicConfig(filename=pipeline_config['application']['logfile_location']
-                            + str(datetime.now().strftime("%d-%m-%Y_%H:%M:%S"))
-                            + '.log'
-                            , level=logging.INFO
-                            , format='%(asctime)s:%(msecs)03d | %(message)s'
-                            , datefmt='%H:%M:%S')
+    logfile = pipeline_config['application']['logfile_location'] + str(datetime.now().strftime("%d-%m-%Y_%H:%M:%S")) + '.log'
+    logging.basicConfig(level=logging.INFO,
+                        format='%(asctime)s:%(msecs)03d | %(message)s',
+                        datefmt='%H:%M:%S',
+                        handlers=[
+                               logging.FileHandler(logfile),
+                               logging.StreamHandler(sys.stdout)
+                           ])
 
-        logging.info('START RUN - Arguments: %s', args)
+    logging.info('START RUN - Arguments: %s', args)
 
     def connect_to_database():  # Open database connection
         """Open the connection to the database"""
@@ -71,22 +71,21 @@ else:  # execute program if arguments are passed
                                  database=pipeline_config['database']['database'],
                                  user=pipeline_config['database']['user'],
                                  password=pipeline_config['database']['password'])
-            print('Database connection made')
-            if args.log: logging.info('Database connection made')
+            logging.info('Database connection made')
             return connection
 
         except Error as e:
             print(e)
-            if args.log: logging.ERROR(e)
+            logging.ERROR(e)
 
 
     db = connect_to_database()
 
     if args.save or args.all:  # Start saving photos to the database
-        print('Moving images from cache to staging')
+        logging.info('Moving images from cache to staging')
 
         photo_cache = os.listdir(pipeline_config['application']['cache_folder'])
-        print(f'Staging {len(photo_cache)} photos')
+        logging.info(f'\tStaging {len(photo_cache)} images')
         for picture in photo_cache:
             saveImage.save(picture,
                            pipeline_config['application']['cache_folder'],
@@ -94,13 +93,13 @@ else:  # execute program if arguments are passed
                            db)
 
     if args.identify or args.all:
-        print('Identifying images is not yet implemented')
+        logging.info('Identifying images is not yet implemented')
         # TODO: calculate predictions
 
     if args.visits or args.all:  # Calculate visits
-        print('Calculating and assigning visits.')
+        logging.info('Calculating and assigning visits.')
         createVisits.calculate(db)
 
     if args.recap or args.all:  # Send summarizing recap messages
-        print('Sending a summary is not yet implemented')
+        logging.info('Sending a summary is not yet implemented')
         # TODO: sending of a recap to a device
