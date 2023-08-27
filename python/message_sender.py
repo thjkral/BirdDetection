a46e988efe521787f347e84fec6675330cda9e3c
thjkral <thjkral@gmail.com>
2023-08-27 20:34:34 +0200

    Added method for sending images

--- python/message_sender.py
+++ python/message_sender.py
@@ -5,6 +5,7 @@ Module for sending messages to a device.
 
 import json
 import telepot
+from datetime import datetime
 
 
 def read_config():  # Load config
@@ -20,6 +21,12 @@ bot = telepot.Bot(secrets['API'])
 def send_single_message(message):
     bot.sendMessage(secrets['USER_ID'], message)
 
+def send_motion_capture(image):
+    file = open(image, 'rb')
+    bot.sendPhoto(secrets['USER_ID'],
+                  file,
+                  caption=f'Movement spotted at the birdfeeder at: {datetime.now().strftime("%d-%m-%Y %H:%M:%S.%f")}')
+
 
 def send_summary():
     pass
@@ -28,3 +35,6 @@ def send_summary():
 if __name__ == '__main__':
     print('sending a test message')
     #send_single_message('Testing 123')
+
+    print('Send test image')
+    send_motion_capture('/home/tom/Pictures/11-07-2022_23-00-27.jpg')
